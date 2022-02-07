73e9c45b7d1bcf287c11870a25a21e3636e7d0a9
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-02-07 11:27:30 -0300

    feat: add runType feature to local jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,17 @@
 import Jest from '@jest/types'
 
+const runTypeArg = process.argv.find(arg => arg.startsWith('--runType='))
+
+const runType = runTypeArg
+  ? runTypeArg.substring('--runType='.length)
+  : ''
+
 export default <Jest.Config.InitialOptions> {
   rootDir: './',
-  roots: ['<rootDir>/src/', '<rootDir>/tests/'],
+  roots: ['<rootDir>/src/', '<rootDir>/tests/' + runType],
   testEnvironment: 'node',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/index.ts'],
   coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text'],
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
