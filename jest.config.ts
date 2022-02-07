37225c529946de709889d50efd1c3c6ad6881efc
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-02-07 14:37:18 -0300

    chore: change jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,8 @@
 import Jest from '@jest/types'
 
-const runTypeArg = process.argv.find(arg => arg.startsWith('--runType='))
-
-const runType = runTypeArg
-  ? runTypeArg.substring('--runType='.length)
-  : ''
-
 export default <Jest.Config.InitialOptions> {
   rootDir: './',
-  roots: ['<rootDir>/src/', '<rootDir>/tests/' + runType],
+  roots: ['<rootDir>/src/', '<rootDir>/tests/'],
   testEnvironment: 'node',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/index.ts'],
   coverageDirectory: 'coverage',
