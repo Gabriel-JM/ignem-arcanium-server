a1c659e970f0f3ad227ff3303d17507ed114a096
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-02-08 10:49:01 -0300

    build: add module-alias for import paths

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,6 @@
+import 'module-alias/register'
 import { server } from './server/app'
 
 console.log('Server in development...')
 
-server.listen(8000, () => console.log('Server running at 8000')).on('error', console.log)
-
+server.listen(8000, () => console.log('Server running at 8000'))
