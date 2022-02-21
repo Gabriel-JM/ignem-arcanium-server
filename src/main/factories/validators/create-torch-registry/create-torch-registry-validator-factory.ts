119c62c199cc0fa010213c2591d65498bf0a452b
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-02-21 14:50:29 -0300

    feat: add create torch registry validator factory

---
+++ src/main/factories/validators/create-torch-registry/create-torch-registry-validator-factory.ts
@@ -0,0 +1,28 @@
+import { ValidatorComposite } from '@/validation/composites'
+import {
+  RequiredFieldsValidator,
+  TypeValidator,
+  ValueInBetweenValidator
+} from '@/validation/validators'
+
+export function makeCreateTorchRegistryValidator() {
+  const validator = new ValidatorComposite([
+    new RequiredFieldsValidator([
+      'characterName',
+      'torchCount',
+      'torchCharge',
+      'isLit'
+    ]),
+    new TypeValidator({
+      characterName: 'string',
+      torchCount: 'number',
+      torchCharge: 'number',
+      isLit: 'boolean'
+    }),
+    new ValueInBetweenValidator({
+      torchCharge: [0, 6]
+    })
+  ])
+
+  return validator
+}
---
+++ src/main/factories/validators/index.ts
@@ -0,0 +1,1 @@
+export * from './create-torch-registry/create-torch-registry-validator-factory'
