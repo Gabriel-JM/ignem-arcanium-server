d8637598dbe708802aad65b2e56ccee194c4223a
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-03-02 16:34:15 -0300

    feat: add migrations running in server start

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,16 @@
 import 'dotenv-safe/config'
 import 'module-alias/register'
 import { server } from './server/app'
+import { knexConnection } from '@/main/factories/repositories'
 
 const port = process.env.PORT || 8000
 
 console.log('Server in development...')
 
-server.listen(port, () => console.log('Server running at ' + port))
+knexConnection.migrate.latest()
+  .then(() => {
+    server.listen(port, () => console.log('Server running at ' + port))
+  })
+  .catch(err => {
+    console.log('Migrations error', err)
+  })
