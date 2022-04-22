7008f693563c97efd07d952fbf7a9cd442927be2
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-04-22 13:46:32 -0300

    feat: add accounts table migration

---
+++ database/migrations/002-create-table-accounts.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex) {
+  await knex.schema.createTable('accounts', (tableBuilder) => {
+    tableBuilder.string('id').primary().notNullable()
+    tableBuilder.string('name').notNullable()
+    tableBuilder.string('email').notNullable()
+    tableBuilder.string('password').notNullable()
+  })
+}
+
+export async function down(knex: Knex) {
+  await knex.schema.dropTable('accounts')
+}
