3dfd0f804ea185d03d5c18ee14929bac6d4aad02
Gabriel José <gabriel.josemelo@hotmail.com>
2022-04-30 18:03:58 -0300

    feat: create a generic controller

---
+++ src/presentation/controllers/generic/generic-controller.ts
@@ -0,0 +1,15 @@
+import { ok } from '@/presentation/helpers'
+import { Controller, HTTPResponse } from '@/presentation/protocols'
+
+export class GenericController implements Controller {
+  constructor(
+    private readonly usecaseMethod: (...args: unknown[]) => Promise<unknown>,
+    private readonly responseFunction = ok
+  ) {}
+  
+  async handle(params: any): Promise<HTTPResponse> {
+    const response = await this.usecaseMethod(params)
+
+    return this.responseFunction(response)
+  }
+}
---
+++ tests/unit/presentation/controllers/generic/generic-controller.spec.ts
@@ -0,0 +1,40 @@
+import { GenericController } from '@/presentation/controllers'
+import { badRequest, ok } from '@/presentation/helpers'
+
+function makeSut(responseFunction = ok) {
+  const fakeUsecaseMethod = jest.fn(() => Promise.resolve({
+    hello: 'world'
+  }))
+  const sut = new GenericController(fakeUsecaseMethod, responseFunction)
+
+  return {
+    sut,
+    fakeUsecaseMethod
+  }
+}
+
+describe('GenericController', () => {
+  it('should call usecaseMethod with correct values', async () => {
+    const { sut, fakeUsecaseMethod } = makeSut()
+
+    await sut.handle({ data: 1 })
+
+    expect(fakeUsecaseMethod).toHaveBeenCalledWith({ data: 1 })
+  })
+
+  it('should return a ok response by default', async () => {
+    const { sut } = makeSut()
+
+    const response = await sut.handle({ data: 1 })
+
+    expect(response).toEqual(ok({ hello: 'world' }))
+  })
+
+  it('should return the correct injected response type', async () => {
+    const { sut } = makeSut(badRequest)
+
+    const response = await sut.handle({ data: 1 })
+
+    expect(response).toEqual(badRequest({ hello: 'world' }))
+  })
+})
