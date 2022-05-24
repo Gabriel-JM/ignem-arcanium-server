7a5f3da52a5fc5463fab3cba6c885ffd31ad69f4
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-05-24 17:18:34 -0300

    feat: ensure DbLoadAccountByToken throws if token is invalid

--- src/data/usecases/account/db-load-account-by-token.ts
+++ src/data/usecases/account/db-load-account-by-token.ts
@@ -1,11 +1,16 @@
+import { InvalidAccessTokenError } from '@/data/errors'
 import { Decrypter } from '@/data/protocols/cryptography/decrypter'
 import { LoadAccountByToken, LoadAccountByTokenResult } from '@/domain/usecases'
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor(private readonly decrypter: Decrypter) {}
   
-  load(token: string): Promise<LoadAccountByTokenResult> {
-    this.decrypter.decrypt(token)
+  async load(token: string): Promise<LoadAccountByTokenResult> {
+    const accountData = await this.decrypter.decrypt(token)
+
+    if (!accountData) {
+      throw new InvalidAccessTokenError()
+    }
 
     return Promise.resolve({}) as Promise<LoadAccountByTokenResult>
   }
--- tests/unit/data/usecases/account/db-load-account-by-token.spec.ts
+++ tests/unit/data/usecases/account/db-load-account-by-token.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidAccessTokenError } from '@/data/errors'
 import { DbLoadAccountByToken } from '@/data/usecases'
 import { mockDecrypter } from '@/tests/unit/mocks'
 
@@ -18,5 +19,14 @@ describe('DbLoadAccountByToken', () => {
     await sut.load('any_token')
 
     expect(decrypterSpy.decrypt).toHaveBeenCalledWith('any_token')
+  })
+
+  it('should throw an InvalidAccessTokenError if Decrypter returns null', async () => {
+    const { sut, decrypterSpy } = makeSut()
+    decrypterSpy.decrypt.mockResolvedValueOnce(null as any)
+
+    const promise = sut.load('any_token')
+
+    await expect(promise).rejects.toThrowError(new InvalidAccessTokenError())
   })
 })
