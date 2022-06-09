ae538a4150eb9836261ce1d9b35e7d6ee110214b
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-06-09 19:02:44 -0300

    feat: allow authorization header in CORS

--- src/main/server/cors.ts
+++ src/main/server/cors.ts
@@ -3,6 +3,6 @@ import { ServerResponse } from 'node:http'
 export function cors(res: ServerResponse) {
   res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL)
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   res.setHeader('Access-Control-Max-Age', '86400')
 }
