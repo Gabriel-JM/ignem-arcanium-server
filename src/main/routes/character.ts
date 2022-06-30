1f75695c860f5aa96cdebd29aef66125c1c552b8
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-06-30 16:35:08 -0300

    feat: add update characters route

--- src/main/routes/character.ts
+++ src/main/routes/character.ts
@@ -2,12 +2,14 @@ import { adaptRoute } from '@/main/adapters'
 import {
   makeCreateCharacterController,
   makeDeleteCharacterController,
-  makeFindAllCharactersController
+  makeFindAllCharactersController,
+  makeUpdateCharacterController
 } from '@/main/factories/controllers'
 import { Router } from '@/main/server/router'
 
 export function characterRoutes(router: Router) {
-  router.http('post', '/characters', adaptRoute(makeCreateCharacterController()))
   router.http('get', '/characters', adaptRoute(makeFindAllCharactersController()))
+  router.http('post', '/characters', adaptRoute(makeCreateCharacterController()))
+  router.http('put', '/characters/:id', adaptRoute(makeUpdateCharacterController()))
   router.http('delete', '/characters/:id', adaptRoute(makeDeleteCharacterController()))
 }
