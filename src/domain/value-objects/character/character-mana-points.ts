93bcf7f24829da5c03fd517368a9be21c516e926
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-07-08 14:43:29 -0300

    refactor: simplifing character hp and mp calculation

--- src/domain/value-objects/character/character-mana-points.ts
+++ src/domain/value-objects/character/character-mana-points.ts
@@ -2,17 +2,7 @@ export class CharacterManaPoints {
   value: number
 
   constructor(level: number, intelligence: number) {
-    this.value = this.#calculateManaPoints(level, intelligence)
-  }
-
-  #calculateManaPoints(level: number, intelligence: number) {
     const initialMP = 10
-    let mp = initialMP
-
-    for (let n=1; n <= level; n++) {
-      mp += intelligence
-    }
-
-    return mp
+    this.value = level * intelligence + initialMP
   }
 }
