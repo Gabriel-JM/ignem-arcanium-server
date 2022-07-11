faaf003de5713ad57e40957c2dad82a330359ac6
Gabriel José <gabriel.josemelo@hotmail.com>
2022-07-11 20:44:41 -0300

    feat: create items table migration

---
+++ database/migrations/0004-create-table-items.ts
@@ -0,0 +1,19 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex) {
+  await knex.schema.createTable('items', tableBuilder => {
+    tableBuilder.string('id').primary().notNullable()
+    tableBuilder.string('name').notNullable()
+    tableBuilder.string('type').notNullable()
+    tableBuilder.text('description').notNullable()
+    tableBuilder.integer('price').notNullable()
+    tableBuilder.integer('weight').notNullable()
+    tableBuilder.string('rarity').notNullable()
+    tableBuilder.integer('charges').defaultTo(0)
+    tableBuilder.jsonb('requirements')
+  })
+}
+
+export async function down(knex: Knex) {
+  await knex.schema.dropTable('items')
+}
