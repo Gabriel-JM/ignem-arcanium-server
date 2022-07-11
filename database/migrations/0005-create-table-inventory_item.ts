8661181e670f2d1234cf2d9f64c72c9e4fb012af
Gabriel José <gabriel.josemelo@hotmail.com>
2022-07-11 20:50:15 -0300

    feat: create inventory_item table migration

---
+++ database/migrations/0005-create-table-inventory_item.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex) {
+  await knex.schema.createTable('inventory_item', tableBuilder => {
+    tableBuilder.string('inventory_id').references('id').inTable('inventories')
+    tableBuilder.string('item_id').references('id').inTable('item')
+  })
+}
+
+export async function down(knex: Knex) {
+  await knex.schema.dropTable('inventory_item')
+}
