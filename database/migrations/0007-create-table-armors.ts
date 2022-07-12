c627e409926a32c1cc85cf9051d8992cd1a6bb9a
Gabriel José <gabriel.josemelo@hotmail.com>
2022-07-11 21:03:42 -0300

    feat: create armors table migration

--- database/migrations/0007-create-table-armors.ts
+++ database/migrations/0007-create-table-armors.ts
@@ -1,18 +1,17 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex) {
-  await knex.schema.createTable('weapons', tableBuilder => {
+  await knex.schema.createTable('armors', tableBuilder => {
     tableBuilder.string('id').primary().notNullable()
     tableBuilder.string('item_id').references('id').inTable('items')
     tableBuilder.string('inventory_id').references('id').inTable('inventories')
-    tableBuilder.string('damage').notNullable()
+    tableBuilder.string('damage_reduction').notNullable()
     tableBuilder.jsonb('damage_type').notNullable()
     tableBuilder.jsonb('properties').notNullable()
     tableBuilder.integer('initiative_modifier').notNullable()
-    tableBuilder.integer('distance').notNullable()
   })
 }
 
 export async function down(knex: Knex) {
-  await knex.schema.dropTable('weapons')
+  await knex.schema.dropTable('armors')
 }
