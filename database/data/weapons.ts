369197fc9a5502fef6a401eb38cea52f78a29c75
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-07-12 11:05:27 -0300

    feat: add more weapons data

--- database/data/weapons.ts
+++ database/data/weapons.ts
@@ -6,7 +6,7 @@ export function makeWeapons() {
       itemId: nanoid(),
       weaponId: nanoid(),
       name: 'Dagger',
-      description: 'Deadly short range blades',
+      description: 'Deadly short range blade',
       damage: 'STR/DEX+1',
       damage_type: '["SLASH","PIERCE"]',
       properties: '["THROWABLE","LIGHT","PRECISION"]',
@@ -39,6 +39,58 @@ export function makeWeapons() {
       initiative_modifier: 0,
       price: 10,
       weight: 45,
+      distance: 0
+    },
+    {
+      itemId: nanoid(),
+      weaponId: nanoid(),
+      name: 'Long Sword',
+      description: 'Most common and versatile sword used by warriors and kings',
+      damage: 'STR+3',
+      damage_type: '["SLASH","PIERCE"]',
+      properties: '["RESISTANT"]',
+      initiative_modifier: -2,
+      price: 14,
+      weight: 70,
+      distance: 0
+    },
+    {
+      itemId: nanoid(),
+      weaponId: nanoid(),
+      name: 'Great Sword',
+      description: 'A really long blade sword, that most be used with two hands and sometimes',
+      damage: 'STR+4',
+      damage_type: '["SLASH","PIERCE"]',
+      properties: '["RESISTANCE","TWO_HANDS","HEAVY"]',
+      initiative_modifier: -4,
+      price: 18,
+      weight: 105,
+      distance: 0
+    },
+    {
+      itemId: nanoid(),
+      weaponId: nanoid(),
+      name: 'Rapier',
+      description: 'A straight sword with a narrow pointed blade',
+      damage: 'STR/DEX+2',
+      damage_type: '["PIERCE"]',
+      properties: '["LIGHT"]',
+      initiative_modifier: 0,
+      price: 12,
+      weight: 40,
+      distance: 0
+    },
+    {
+      itemId: nanoid(),
+      weaponId: nanoid(),
+      name: 'Rapier',
+      description: 'A straight sword with a narrow pointed blade',
+      damage: 'STR/DEX+2',
+      damage_type: '["PIERCE"]',
+      properties: '["LIGHT"]',
+      initiative_modifier: 0,
+      price: 12,
+      weight: 40,
       distance: 0
     }
   ]
