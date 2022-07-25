a0f8feabae4b02b3dd022d4e32494074f7fbbff4
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-07-25 19:32:43 -0300

    feat: create migration to add consumable items

---
+++ database/data/consumable-items.ts
@@ -0,0 +1,90 @@
+export function makeConsumableItems() {
+  return [
+    {
+      itemId: 'Oph1O_HBecmd6VdEZLGB7',
+      name: 'Tent',
+      description: 'A simple and portable shelter that sleeps four people',
+      price: 12,
+      weight: 35
+    },
+    {
+      itemId: '0M6FEfl6w95w6ot_hk8j8',
+      name: 'Rope',
+      description: 'The rope is made of hemp, cotton, linen or silk, it has 2HP and can be'
+        + ' broken with a success strength test CD 11. Its imune to strike damage',
+      price: 2,
+      weight: 5
+    },
+    {
+      itemId: 'c7KSjlPy4ga_kAlF9umcu',
+      name: 'Oil',
+      description: 'It usually comes in a glass or clay bottle that contains 500ml.'
+        + ' This item can be thrown at a target, scattered on the ground, used as'
+        + ' fuel, among other actions.\n'
+        + 'Effects:\n'
+        + ' Flammable\n'
+        + ' - If a target is covered in oil and taking some form of fire damage,'
+        + ' it ignites for an additional 4 points of fire damage.\n'
+        + ' - If accessing any surface any character in the location takes'
+        + ' 4 fire damage per turn at the location, the flames last for 2 rounds.',
+      price: 5,
+      weight: 10
+    },
+    {
+      itemId: 'kYRdbBnh3j3jCeRu-WZW5',
+      name: 'Crowbar',
+      description: 'Using a crowbar grants a +4 advantage on Strength checks where'
+        + ' it makes sense to apply it.',
+      price: 8,
+      weight: 5
+    },
+    {
+      itemId: '68pTat2YeTfp8fnJpOaZb',
+      name: 'Darts and Arrows',
+      description: 'Ammunition for long distance weapons, arrows for bows and darts for crossbows',
+      price: 1,
+      weight: 1
+    },
+    {
+      itemId: 'V7Y93ZTbculsVuQ2nEim-',
+      name: 'Travel Rations',
+      description: 'A pack of various travel-friendly dehydrated foods, including jerky,'
+        + ' dried fruit, crackers and nuts. On average 2 is used a day.',
+      price: 5,
+      weight: 10
+    },
+    {
+      itemId: 'A5i_xfOAGVi_YSLZNatw6',
+      name: 'Throwing Dagger',
+      description: 'Small daggers specifically for throwing, deal 1d6+(Dexterity) damage.',
+      price: 1,
+      weight: 3
+    },
+    {
+      itemId: 'YkbO-kPebT7tgnvhzqm11',
+      name: 'Torch',
+      description: 'The torch burns for 2 hours providing light. If you hit a melee attack'
+        + ' with a torch, it deals 1 fire damage. If the torch falls to the ground, make a'
+        + ' DC 8 non-attribute check, failing which the torch goes out. Torches can be'
+        + ' considered extra weapons.',
+      price: 5,
+      weight: 15
+    },
+    {
+      itemId: 'DDWLUuvJgLgt6TO6YaF4s',
+      name: 'Bandage',
+      description: 'Bandages used to stop bleeding. With DC 8 Medicine(WIS). If it fails,'
+        + ' the bandage wears out and grants a target reduction of the bleeding effect.',
+      price: 8,
+      weight: 5
+    },
+    {
+      itemId: 'hhoZ3wO-vEF3SvYHiI_m7',
+      name: 'Lockpicks',
+      description: 'Small shank tool, usually made of iron, that can be used to carry'
+        + ' out work in small spaces such as locks.',
+      price: 1,
+      weight: 1
+    }
+  ]
+}
---
+++ database/migrations/0014-insert-default-consumable-items.ts
@@ -0,0 +1,24 @@
+import { Knex } from 'knex'
+import { makeConsumableItems } from '../data/consumable-items'
+
+const consumables = makeConsumableItems()
+
+export async function up(knex: Knex) {
+  for (const data of consumables) {
+    await knex('items').insert({
+      id: data.itemId,
+      name: data.name,
+      type: 'CONSUMABLE',
+      description: data.description,
+      price: data.price,
+      weight: data.weight,
+      rarity: 'COMMON'
+    })
+  }
+}
+
+export async function down(knex: Knex) {
+  for (const { itemId } of consumables) {
+    await knex('items').where({ id: itemId }).delete()
+  }
+}
