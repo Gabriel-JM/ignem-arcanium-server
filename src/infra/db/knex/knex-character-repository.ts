04fda964b23188db0a57aec2044c84ceeca2668e
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-08-29 17:04:16 -0300

    feat: add inventory items insert on character creation

--- src/infra/db/knex/knex-character-repository.ts
+++ src/infra/db/knex/knex-character-repository.ts
@@ -1,3 +1,4 @@
+import { UniqueIdGenerator } from '@/data/protocols/identification'
 import {
   CheckCharacterRepository,
   CheckCharacterRepositoryParams,
@@ -20,8 +21,13 @@ type Repository = CreateCharacterRepository
 
 export class KnexCharacterRepository implements Repository {
   tableName = 'characters'
+  #knexHelper: KnexHelper
+  #uniqueIdGenerator: UniqueIdGenerator
 
-  constructor(private readonly knexHelper: KnexHelper) {}
+  constructor(knexHelper: KnexHelper, uniqueIdGenerator: UniqueIdGenerator) {
+    this.#knexHelper = knexHelper
+    this.#uniqueIdGenerator = uniqueIdGenerator
+  }
 
   #mapFields(dbData: DbCharacter) {
     const {
@@ -38,9 +44,9 @@ export class KnexCharacterRepository implements Repository {
   }
 
   async check({ id, accountId }: CheckCharacterRepositoryParams) {
-    const exists = await this.knexHelper
+    const exists = await this.#knexHelper
       .table(this.tableName)
-      .select(this.knexHelper.conn.raw('1'))
+      .select(this.#knexHelper.conn.raw('1'))
       .where({ id, account_id: accountId })
       .first()
 
@@ -48,7 +54,7 @@ export class KnexCharacterRepository implements Repository {
   }
   
   async findAll(accountId: string) {
-    const characters = await this.knexHelper
+    const characters = await this.#knexHelper
       .table(this.tableName)
       .select<DbCharacter[]>()
       .where({ account_id: accountId })
@@ -57,8 +63,8 @@ export class KnexCharacterRepository implements Repository {
   }
 
   async create(params: CreateCharacterRepositoryParams): Promise<void> {
-    await this.knexHelper.transaction(async trx => {
-      await this.knexHelper
+    await this.#knexHelper.transaction(async trx => {
+      await this.#knexHelper
         .table(this.tableName)
         .insert({
           id: params.id,
@@ -82,25 +88,39 @@ export class KnexCharacterRepository implements Repository {
         })
         .transacting(trx)
 
-      await this.knexHelper
+      await this.#knexHelper
         .table('inventories')
         .insert({
           id: params.inventoryId,
           size: 200
         })
         .transacting(trx)
+
+      if (params.inventoryItems) {
+        await this.#knexHelper
+          .table('inventory_item')
+          .insert(params.inventoryItems.map(item => {
+            return {
+              id: this.#uniqueIdGenerator.generate(),
+              inventoryId: params.inventoryId,
+              itemId: item.itemId,
+              quantity: item.quantity
+            }
+          }))
+          .transacting(trx)
+      }
     })
   }
 
   async delete(params: DeleteCharacterRepositoryParams) {
-    await this.knexHelper
+    await this.#knexHelper
       .table(this.tableName)
       .where({ id: params.id, account_id: params.accountId })
       .delete()
   }
 
   async update({ id, accountId, ...data }: UpdateCharacterRepositoryParams) {
-    await this.knexHelper
+    await this.#knexHelper
       .table(this.tableName)
       .where({ id, account_id: accountId })
       .update(data)
