3a1c0566d822beb576f4f2abd7fa08196cd3589d
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-11-23 14:12:20 -0300

    feat: enable null values in optional fields

--- tests/unit/validation/validators/required-fields-validator.spec.ts
+++ tests/unit/validation/validators/required-fields-validator.spec.ts
@@ -18,6 +18,20 @@ describe('RequiredFieldsValidator', () => {
     ])
   })
 
+  it('should return an array of missing fields if is set to null', () => {
+    const sut = makeSut(['field1', 'field2'])
+
+    const response = sut.validate({
+      field1: null,
+      field2: null
+    })
+
+    expect(response).toEqual([
+      'field1 is required',
+      'field2 is required'
+    ])
+  })
+
   it('should return an empty array if all fields exists', () => {
     const sut = makeSut(['field'])
 
