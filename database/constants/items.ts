2bebc4ee56e4d5fda5eb45168d31bf4a7b337ba2
Gabriel José <gabriel.josemelo@hotmail.com>
2022-12-06 22:43:16 -0300

    feat: add all items sub types

---
+++ database/constants/items.ts
@@ -0,0 +1,49 @@
+export const ItemTypes = {
+  weapon: 'weapon',
+  shield: 'shield',
+  armor: 'armor',
+  alchemicalItem: 'alchemicalItem',
+  gem: 'gem',
+  consumable: 'consumable'
+}
+
+export const WeaponSubTypes = {
+  dagger: 'dagger',
+  staff: 'staff',
+  shortSword: 'shortSword',
+  longSword: 'longSword',
+  greatSword: 'greatSword',
+  rapier: 'rapier',
+  axe: 'axe',
+  greatAxe: 'greatAxe',
+  hammer: 'hammer',
+  greatHammer: 'greatHammer',
+  shortBow: 'shortBow',
+  bow: 'bow',
+  longBow: 'longBow',
+  handCrossbow: 'handCrossbow',
+  crossbow: 'crossbow'
+}
+
+export const ShieldSubTypes = {
+  buckler: 'buckler',
+  lightShield: 'lightShield',
+  mediumShield: 'mediumShield',
+  greatShield: 'greatShield'
+}
+
+export const armorSubTypes = {
+  cloth: 'cloth',
+  lightArmor: 'lightArmor',
+  mediumArmor: 'mediumArmor',
+  heavyArmor: 'heavyArmor'
+}
+
+export const alchemicalItemSubTypes = {
+  potion: 'potion',
+  elixir: 'elixir',
+  ointment: 'ointment',
+  poison: 'poison',
+  oil: 'oil',
+  gas: 'gas'
+}
