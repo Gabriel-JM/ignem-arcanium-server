02007be0ece1b484a91938657f4c19ae6b34616d
Gabriel Jose <gabriel.josemelo@hotmail.com>
2023-06-21 10:11:19 -0300

    feat: add prisma migration on build

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,27 +1,14 @@
 import 'dotenv-safe/config.js'
 import { server } from './server/app.js'
-import { knexConnection } from '@/main/factories/repositories/index.js'
 
 const port = process.env.PORT || 8000
 
 console.log('Server in development...')
 
-let retries = 5
+async function start() {
+  await import('./config/prisma.js')
 
-function start() {
-  knexConnection.migrate.latest()
-  .then(() => {
-    server.listen(port, () => console.log('Server running at ' + port))
-  })
-  .catch(err => {
-    if (retries == 0) {
-      return console.log('Migrations error', err)
-    }
-
-    retries--
-    console.log('Retrying... retries left:', retries)
-    setTimeout(start, 1500)
-  })
+  server.listen(port, () => console.log('Server running at ' + port))
 }
 
 start()
