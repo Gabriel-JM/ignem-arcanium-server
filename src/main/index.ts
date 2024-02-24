893174e48335357c39c3b43dbf77472a03bdd10d
Gabriel José <gabriel.josemelo@hotmail.com>
2024-02-23 22:36:02 -0300

    feat: remove prisma and return to use knex

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,14 +1,18 @@
 import 'dotenv-safe/config.js'
 import { server } from './server/app.js'
+import { knexConnection } from './factories/repositories/knex-connection.js'
 
 const port = process.env.PORT || 8000
 
 console.log('Server in development...')
 
-async function start() {
-  await import('./config/prisma.js')
-
-  server.listen(port, () => console.log('Server running at ' + port))
-}
-
-start()
+knexConnection
+  .migrate
+  .latest()
+  .then(() => {
+    server.listen(
+      port,
+      () => console.log('Server running http://localhost:' + port)
+    )
+  })
+  .catch(error => console.log('Migration Init error', error))
